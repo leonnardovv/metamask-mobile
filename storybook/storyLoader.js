0632dc3c295164e19857680023932200b17d88b5
georgewrmarshall <george.marshall@consensys.net>
2022-01-12 15:01:09 +1300

    Updating button story to styled button

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -7,16 +7,16 @@ function loadStories() {
 	require('../app/components/Base/Alert.stories');
 	require('../app/components/Base/Text.stories');
 	require('../app/components/Base/Title.stories');
-	require('../app/components/UI/Button/Button.stories');
 	require('../app/components/UI/Fox/Fox.stories');
+	require('../app/components/UI/StyledButton/StyledButton.stories');
 }
 
 const stories = [
 	'../app/components/Base/Alert.stories',
 	'../app/components/Base/Text.stories',
 	'../app/components/Base/Title.stories',
-	'../app/components/UI/Button/Button.stories',
 	'../app/components/UI/Fox/Fox.stories',
+	'../app/components/UI/StyledButton/StyledButton.stories',
 ];
 
 module.exports = {
